4d0d0efa17b9871457d72a6f4f75a38b3517409c
Blessiey <mogashoablessing676@gmail.com>
2024-09-27 15:10:09 +0200

    Commit

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,23 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore/lite";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore/lite";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBLiSWDogxnHgOYW7DC05UmEVv_O-sniFY",
-    authDomain: "myapp-fe0a5.firebaseapp.com",
-    projectId: "myapp-fe0a5",
-    storageBucket: "myapp-fe0a5.appspot.com",
-    messagingSenderId: "185797597842",
-    appId: "1:185797597842:web:b90942491af1bec0fc244b"
+// const firebaseConfig = {
+
+// };
 
 
-};
-
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-export { auth, db }
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+// export { auth, db }
